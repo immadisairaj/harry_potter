8d43a801bb24fba1517452841684daab1c437f36
immadisairaj <immadirajendra.sai@gmail.com>
2019-07-17 20:14:56 +0530

    harry_potter with the main UI

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'hat/hat_route.dart';
+import 'characters/characters_route.dart';
+//import 'houses/houses_route.dart';
+//import 'spells/spells_route.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(HarryPotterApp());
+  });
+}
+
+class HarryPotterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: HomeScreen());
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Harry Potter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        // primaryColor: Color(0x0b0630),
+        accentColor: Colors.amber,
+        fontFamily: 'Montserrat',
+      ),
+      home: Container (
+        decoration: BoxDecoration (
+          image: DecorationImage (
+            image: AssetImage('assets/images/hogwarts.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center (
+                child: Text (
+                  'Harry Potter App',
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    color: Colors.amber,
+                  ),
+                ),
+              ),
+              Center (
+                child: Opacity (
+                  opacity: 0.7,
+                  child: SizedBox (
+                    width: 200.0,
+                    height: 80.0,
+                    child: RaisedButton (
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      color: Colors.blue[900],
+                      child: Text(
+                        "Hat",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Todo: add hat
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Center (
+                child: Opacity (
+                  opacity: 0.7,
+                  child: SizedBox (
+                    width: 200.0,
+                    height: 80.0,
+                    child: RaisedButton (
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      color: Colors.blue[900],
+                      child: Text(
+                        "Characters",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Todo: add characters
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Center (
+                child: Opacity (
+                  opacity: 0.7,
+                  child: SizedBox (
+                    width: 200.0,
+                    height: 80.0,
+                    child: RaisedButton (
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      color: Colors.blue[900],
+                      child: Text(
+                        "Houses",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Todo: add houses
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Center (
+                child: Opacity (
+                  opacity: 0.7,
+                  child: SizedBox (
+                    width: 200.0,
+                    height: 80.0,
+                    child: RaisedButton (
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                      color: Colors.blue[900],
+                      child: Text(
+                        "Spells",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        // Todo: add spells
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
